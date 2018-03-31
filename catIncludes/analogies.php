7014488f5d25ab8f5e8d0272ac484f722913a1b0
Tim <tferris88@gmail.com>
2018-03-30 20:01:05 -0400

    still don't know what I'm doing...

---
+++ catIncludes/analogies.php
@@ -0,0 +1,51 @@
+<section class="resume-section p-3 p-lg-5 d-flex flex-column" id="experience">
+        <div class="my-auto">
+          <h2 class="mb-5">Analogies</h2>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Analogy1</h3>
+              <div class="subheading mb-3">Analogy title</div>
+              <p>explanation</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">something</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Analogy Title</h3>
+              <div class="subheading mb-3">Analogy title 2.0</div>
+              <p>analogy explanation</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">something</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">analogy title</h3>
+              <div class="subheading mb-3">analogy subtitle</div>
+              <p>analogy explanation</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">something</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Analogy title</h3>
+              <div class="subheading mb-3">analogy something</div>
+              <p>analogy explanation</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">something</span>
+            </div>
+          </div>
+
+        </div>
+
+      </section>
---
+++ catIncludes/icons.php
@@ -0,0 +1,36 @@
+<div class="subheading mb-3">Programming Languages &amp; Tools</div>
+          <ul class="list-inline list-icons">
+            <li class="list-inline-item">
+              <i class="devicons devicons-html5"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-css3"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-javascript"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-jquery"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-sass"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-less"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-bootstrap"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-wordpress"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-grunt"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-gulp"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-npm"></i>
+            </li>
+          </ul>
