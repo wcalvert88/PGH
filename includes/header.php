a912468c4e73bcfa24cc053dfeac90375ca77816
wcalvert88 <wcalvert88@gmail.com>
2018-03-30 20:30:15 -0400

    got basic outline of main page

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Round About - Start Bootstrap Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/round-about.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css">
+  </head>
+
+  <body>
--- index.php
+++ index.php
@@ -1,126 +1,66 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php include "includes/header.php"; ?>
+<?php include "includes/navbar.php"; ?>
 
-  <head>
+<!-- Page Content -->
+<div class="container">
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+  <!-- Introduction Row -->
+  <h1 class="my-4 text-center"><u>The Programmers Guide To Humans</u><div class="form-group">
+  <small>This guide will teach you how to talk to mere mortals</small></div>
+  </h1>
+  <p class="text-center">Choose A Category To Learn How To Talk To Your Non-Programmer Friends</p>
 
-    <title>Round About - Start Bootstrap Template</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="css/round-about.css" rel="stylesheet">
-
-  </head>
-
-  <body>
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="#">Start Bootstrap</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home
-                <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Services</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
-    <!-- Page Content -->
-    <div class="container">
-
-      <!-- Introduction Row -->
-      <h1 class="my-4">About Us
-        <small>It's Nice to Meet You!</small>
-      </h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, explicabo dolores ipsam aliquam inventore corrupti eveniet quisquam quod totam laudantium repudiandae obcaecati ea consectetur debitis velit facere nisi expedita vel?</p>
-
-      <!-- Team Members Row -->
-      <div class="row">
-        <div class="col-lg-12">
-          <h2 class="my-4">Our Team</h2>
-        </div>
-        <div class="col-lg-4 col-sm-6 text-center mb-4">
-          <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
-          <h3>John Smith
-            <small>Job Title</small>
-          </h3>
-          <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-        </div>
-        <div class="col-lg-4 col-sm-6 text-center mb-4">
-          <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
-          <h3>John Smith
-            <small>Job Title</small>
-          </h3>
-          <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-        </div>
-        <div class="col-lg-4 col-sm-6 text-center mb-4">
-          <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
-          <h3>John Smith
-            <small>Job Title</small>
-          </h3>
-          <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-        </div>
-        <div class="col-lg-4 col-sm-6 text-center mb-4">
-          <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
-          <h3>John Smith
-            <small>Job Title</small>
-          </h3>
-          <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-        </div>
-        <div class="col-lg-4 col-sm-6 text-center mb-4">
-          <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
-          <h3>John Smith
-            <small>Job Title</small>
-          </h3>
-          <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-        </div>
-        <div class="col-lg-4 col-sm-6 text-center mb-4">
-          <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
-          <h3>John Smith
-            <small>Job Title</small>
-          </h3>
-          <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-        </div>
-      </div>
-
+  <!-- Team Members Row -->
+  <div class="row">
+    <div class="col-lg-12">
+      <h2 class="my-4">Our Team</h2>
     </div>
-    <!-- /.container -->
+    <div class="col-lg-4 col-sm-6 text-center mb-4">
+      <img class="rounded-circle img-fluid d-block mx-auto" src="images/BPC.jpg" alt="Basic Programming Concepts">
+      <h3>Basic Programming Concepts
+        <!-- <small>Job Title</small> -->
+      </h3>
+      <!-- <p>What does this team member to? Keep it short! This is also a great spot for social links!</p> -->
+    </div>
+    <div class="col-lg-4 col-sm-6 text-center mb-4">
+      <i class="devicon-html5-plain-wordmark colored" style="font-size:10rem"></i>
+      <h3>HTML 5
+        <!-- <small>Job Title</small> -->
+      </h3>
+      <!-- <p>What does this team member to? Keep it short! This is also a great spot for social links!</p> -->
+    </div>
+    <div class="col-lg-4 col-sm-6 text-center mb-4">
+      <i class="devicon-css3-plain-wordmark colored" style="font-size:10rem"></i>
+      <!-- <img class="rounded-circle img-fluid d-block mx-auto" src="images/CSS3.jpg" alt="CSS3"> -->
+      <h3>CSS3
+        <!-- <small>Job Title</small> -->
+      </h3>
+      <!-- <p>What does this team member to? Keep it short! This is also a great spot for social links!</p> -->
+    </div>
+    <div class="col-lg-4 col-sm-6 text-center mb-4">
+      <img class="rounded-circle img-fluid d-block mx-auto" src="images/JavaScript.jpg" style="height:10rem;width:10rem" alt="JavaScript">
+      <h3>John Smith
+        <!-- <small>Job Title</small> -->
+      </h3>
+      <!-- <p>What does this team member to? Keep it short! This is also a great spot for social links!</p> -->
+    </div>
+    <div class="col-lg-4 col-sm-6 text-center mb-4">
+    <i class="devicon-nodejs-plain-wordmark colored" style="font-size:10rem"></i>
 
-    <!-- Footer -->
-    <footer class="py-5 bg-dark">
-      <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2018</p>
-      </div>
-      <!-- /.container -->
-    </footer>
+      <h3>John Smith
+        <!-- <small>Job Title</small> -->
+      </h3>
+      <!-- <p>What does this team member to? Keep it short! This is also a great spot for social links!</p> -->
+    </div>
+    <div class="col-lg-4 col-sm-6 text-center mb-4">
+    <i class="devicon-php-plain colored" style="font-size:10rem;"></i>
+      <h3>John Smith
+        <!-- <small>Job Title</small> -->
+      </h3>
+      <!-- <p>What does this team member to? Keep it short! This is also a great spot for social links!</p> -->
+    </div>
+  </div>
 
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  </body>
-
-</html>
+</div>
+<!-- /.container -->
+<?php include "includes/footer.php"; ?>
