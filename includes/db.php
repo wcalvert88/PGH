d294ca6708978e8c6c9329d47e49d8e4c19fd2d4
Tim <tferris88@gmail.com>
2018-04-13 21:31:03 -0400

    something

---
+++ includes/db.php
@@ -0,0 +1,16 @@
+<?php ob_start();
+
+$db['db_host'] = 'localhost';
+$db['db_user'] = 'root';
+$db['db_pass'] = 'root';
+$db['db_name'] = 'pgh';
+
+foreach($db as $key => $value) {
+   define(strtoupper($key), $value);
+}
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+$query = "SET NAMES utf8";
+mysqli_query($connection, $query);
+?>
